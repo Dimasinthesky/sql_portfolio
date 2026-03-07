f0a453b49e4c7b509b8059489f24f58818c585d7
Dimasinthesky <kroitor.dima08@gmail.com>
2026-03-07 17:24:29 +0100

    Add SQL query for monthly revenue calculation

---
+++ sql/03_revenue_by_month.sql
@@ -0,0 +1,8 @@
+SELECT 
+    strftime('%Y', o.order_purchase_timestamp) AS year,
+    strftime('%m', o.order_purchase_timestamp) AS month,
+    SUM(oi.price) AS revenue
+FROM order_items oi
+JOIN orders o ON oi.order_id = o.order_id
+GROUP BY year, month
+ORDER BY year, month;
