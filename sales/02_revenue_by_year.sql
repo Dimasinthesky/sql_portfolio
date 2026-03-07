91702c6a6c83295d47b44c864e25a80f432ab217
Dimasinthesky <kroitor.dima08@gmail.com>
2026-03-07 23:42:15 +0100

    Update 02_revenue_by_year.sql

--- sales/02_revenue_by_year.sql
+++ sales/02_revenue_by_year.sql
@@ -1,1 +1,10 @@
+-- Revenue by year
 
+SELECT 
+    STRFTIME('%Y', o.order_purchase_timestamp) AS year,
+    SUM(oi.price) AS revenue
+FROM order_items oi
+JOIN orders o 
+    ON oi.order_id = o.order_id
+GROUP BY year
+ORDER BY year DESC;
