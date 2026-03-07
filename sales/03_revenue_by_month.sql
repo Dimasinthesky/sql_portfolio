f7f2b9c7d70e662d2aced2c534438dff0f6448aa
Dimasinthesky <kroitor.dima08@gmail.com>
2026-03-08 00:16:14 +0100

    Update 03_revenue_by_month.sql

--- sales/03_revenue_by_month.sql
+++ sales/03_revenue_by_month.sql
@@ -1,1 +1,11 @@
+-- Revenue by month
 
+SELECT 
+    STRFTIME('%Y', o.order_purchase_timestamp) AS year,
+    STRFTIME('%m', o.order_purchase_timestamp) AS month,
+    SUM(oi.price) AS revenue
+FROM order_items oi
+JOIN orders o 
+    ON oi.order_id = o.order_id
+GROUP BY year, month
+ORDER BY year, month;
