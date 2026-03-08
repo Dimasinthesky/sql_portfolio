71da700558d1dda5b3cc5efd40a873a3e5eb434b
Dimasinthesky <kroitor.dima08@gmail.com>
2026-03-08 02:11:02 +0100

    Update 09_orders_count_by_month.sql

--- sales/09_orders_count_by_month.sql
+++ sales/09_orders_count_by_month.sql
@@ -1,1 +1,8 @@
 
+SELECT 
+    strftime('%Y', order_purchase_timestamp) AS year,
+    strftime('%m', order_purchase_timestamp) AS month,
+    COUNT(order_id) AS orders
+FROM orders
+GROUP BY year, month
+ORDER BY year, month;
