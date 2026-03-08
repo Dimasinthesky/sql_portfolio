e29cba919bb9f80f770da63471e903354b6920b0
Dimasinthesky <kroitor.dima08@gmail.com>
2026-03-08 02:05:52 +0100

    Update 07_avg_order_value.sql

--- sales/07_avg_order_value.sql
+++ sales/07_avg_order_value.sql
@@ -1,1 +1,13 @@
-
+--AOV
+WITH sum_order AS (
+    SELECT 
+        o.order_id,
+        SUM(oi.price) AS revenue
+    FROM order_items oi
+    JOIN orders o 
+        ON oi.order_id = o.order_id
+    GROUP BY o.order_id
+)
+SELECT 
+    AVG(revenue) AS avg_order_value
+FROM sum_order;
