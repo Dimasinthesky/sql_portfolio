7f188ba7a6fd266bc7434711b41c87d7d7e07eb9
Dimasinthesky <kroitor.dima08@gmail.com>
2026-03-08 01:24:51 +0100

    Update 06_revenue_by_payment_type.sql

--- sales/06_revenue_by_payment_type.sql
+++ sales/06_revenue_by_payment_type.sql
@@ -1,1 +1,9 @@
-
+SELECT 
+    op.payment_type,
+    SUM(oi.price) AS revenue
+FROM order_payments op
+JOIN orders o 
+    ON op.order_id = o.order_id
+JOIN order_items oi 
+    ON o.order_id = oi.order_id
+GROUP BY op.payment_type;
