2e615e2b8100af40cd261dc0be599963ab344e1e
Dimasinthesky <kroitor.dima08@gmail.com>
2026-03-08 01:14:40 +0100

    Update 05_revenue_by_state.sql

--- sales/05_revenue_by_state.sql
+++ sales/05_revenue_by_state.sql
@@ -1,1 +1,12 @@
+-- Revenue by customer state
 
+SELECT 
+    c.customer_state AS state,
+    SUM(oi.price) AS revenue
+FROM customers c
+JOIN orders o 
+    ON c.customer_id = o.customer_id
+JOIN order_items oi 
+    ON o.order_id = oi.order_id
+GROUP BY state
+ORDER BY revenue DESC;
