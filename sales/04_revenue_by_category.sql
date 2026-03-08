81e50764c462d146906168235edc63127290b4fc
Dimasinthesky <kroitor.dima08@gmail.com>
2026-03-08 01:00:06 +0100

    Update 04_revenue_by_category.sql

--- sales/04_revenue_by_category.sql
+++ sales/04_revenue_by_category.sql
@@ -1,1 +1,11 @@
+-- Revenue by product category
 
+SELECT 
+    p.product_category_name AS category,
+    SUM(oi.price) AS revenue
+FROM products p
+JOIN order_items oi 
+    ON p.product_id = oi.product_id
+WHERE p.product_category_name IS NOT NULL
+GROUP BY category
+ORDER BY revenue DESC;
